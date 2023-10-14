d56692e55f1ef1a6d31a01bc84cc45523262458e
Matt Robinson <matthewrobinsondev@gmail.com>
2023-10-14 11:02:13 +0100

    Update to php 8.2

--- local_tests.sh
+++ local_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./vendor/bin/phpmnd app routes tests config
+./vendor/bin/sail exec php ./vendor/bin/phpmnd app routes tests config
 
-./vendor/bin/phpstan analyse --memory-limit=2G
+./vendor/bin/sail exec php ./vendor/bin/phpstan analyse --memory-limit=2G
 
-./vendor/bin/phpmd app,routes,tests text phpmd.xml
+./vendor/bin/sail exec php ./vendor/bin/phpmd app,routes,tests text phpmd.xml
 
 ./vendor/bin/sail pint
 
