dde7359de17caa1c1249d9aa5934e9f47fc3274c
Matt Robinson <matthewrobinsondev@gmail.com>
2024-01-27 23:25:07 +0000

    Fix Team info

--- app/Http/Requests/TeamStoreRequest.php
+++ app/Http/Requests/TeamStoreRequest.php
@@ -5,9 +5,22 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Modules\Teams\Exceptions\TeamException;
 
 class TeamStoreRequest extends FormRequest
 {
+
+    /**
+     * @throws TeamException
+     */
+    public function authorize(): bool
+    {
+        if ($this->user()->team()->exists()) {
+            throw TeamException::userCantHaveMultipleTeams();
+        }
+
+        return true;
+    }
     /**
      * @return array{name: array{0: string}}
      */
